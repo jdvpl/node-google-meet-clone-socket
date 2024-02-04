67f501e31fdaebf56ab957c38970d8d62a8aa648
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2024-02-04 17:24:13 -0500

    finished project

--- README.md
+++
@@ -1,7 +0,0 @@
-# node-bitcoin-payment
-
-### run ngrok you musht specify port -7600
-
-```
-  ngrok http port
-```
--- config/index.js
+++
@@ -1,9 +0,0 @@
-const COINBASE_API_KEY = process.env.COINBASE_API_KEY;
-const COINBASE_WEBHOOK_SECRET = process.env.COINBASE_WEBHOOK_SECRET;
-const DOMAIN = "http://localhost:7600";
-
-module.exports = {
-  COINBASE_API_KEY,
-  COINBASE_WEBHOOK_SECRET,
-  DOMAIN,
-};
